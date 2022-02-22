d2c0c577be02b106c74945c76522fdb1c06e7e8f
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-21 16:24:19 -0800

    create function to generate random string

--- express_server.js
+++ express_server.js
@@ -1,6 +1,13 @@
+function generateRandomString() {
+  const result = Math.random().toString(36).substring(2,7);
+  return result;
+}
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080]
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -19,6 +26,15 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls", (req, res) => {
