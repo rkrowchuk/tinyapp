e4672aca74b4881a29a56fce71deb17359c1a766
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-21 17:02:40 -0800

    add redirect to longURL

--- express_server.js
+++ express_server.js
@@ -40,6 +40,11 @@ app.post("/urls", (req, res) => {
   //res.send('Ok');         // Respond with 'Ok' (we will replace this)
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
