0fd0186d4f5f3387c97a3094f7789a09ca8f6981
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-22 18:02:57 -0800

    add login feature within header

--- express_server.js
+++ express_server.js
@@ -7,8 +7,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080]
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const { status } = require("express/lib/response");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser({extended: false}));
 
 app.set("view engine", "ejs");
 
@@ -30,7 +32,10 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"] 
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -51,16 +56,26 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"] 
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL] 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"] 
   };
   res.render("urls_show", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
